286be38cffeba0dcf63a575d65e78d6333b5be69
EC2 Default User <ec2-user@ip-172-31-22-250.us-east-2.compute.internal>
2023-06-26 22:08:20 +0000

    Finish integration with Dolly

--- src/rest_api/server.py
+++ src/rest_api/server.py
@@ -1,8 +1,21 @@
 import  socket
+
+from transformers import pipeline
+import torch
+
 def server_program():
     # get the hostname
-    host = socket.gethostname()
-    port = 5000  # initiate port no above 1024
+    #host = socket.gethostname()
+    host = "172.31.22.250"
+    port = 80 # initiate port no above 1024
+    print("host",host)
+
+    instruct_pipeline = pipeline(model="databricks/dolly-v2-3b",
+                                 torch_dtype=torch.bfloat16,
+                                 trust_remote_code=True,
+                                 device_map="auto")
+    print("Start")
+
 
     server_socket = socket.socket()  # get instance
     # look closely. The bind() function takes tuple as argument
@@ -14,13 +27,17 @@ def server_program():
     print("Connection from: " + str(address))
     while True:
         # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
+        data = conn.recv(10240).decode()
         if not data:
             # if data is not received break
+            print("Void data received")
             break
         print("from connected user: " + str(data))
-        data = input(' -> ')
-        conn.send(data.encode())  # send data to the client
+        income = str(data)
+        res = instruct_pipeline(income)
+        print(res[0]["generated_text"])
+        data_out = res[0]["generated_text"].encode()
+        conn.send(data_out)  # send data to the client
 
     conn.close()  # close the connection
 
