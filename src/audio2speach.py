31bee50e1c89a7e179aa4d21f13fff733c1f273c
leopard <you@example.com>
2023-06-26 20:04:34 +0300

    Speach to audio example

---
+++ src/audio2speach.py
@@ -0,0 +1,24 @@
+
+# For managing audio file
+import librosa
+import torch
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
+
+def main():
+    tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
+    model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+
+    audio, rate = librosa.load("data/example.wav", sr=16000)
+    print(audio)
+    print(rate)
+
+    input_values = tokenizer(audio, return_tensors="pt").input_values
+    logits = model(input_values).logits
+    prediction = torch.argmax(logits, dim=-1)
+    transcription = tokenizer.batch_decode(prediction)[0]
+    print(transcription)
+
+    pass
+
+if __name__ == '__main__':
+    main()
