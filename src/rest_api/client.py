084774ee7643c6a66d0deea878a604531d418a56
Eden Mikitas <e.mikitas@gmail.com>
2023-06-27 00:45:28 +0300

    client to class

--- src/rest_api/client.py
+++ src/rest_api/client.py
@@ -5,32 +5,35 @@ host = socket.gethostname()  # as both code is running on same pc
 port = 5000
 client_socket = None
 
-
-def init_client():
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
-
-
-def query_dolly(prompt):
-    client_socket.send(prompt.encode())
-    data = client_socket.recv(1024).decode()  # receive response
-
-    return data
-
-def client_program():
+class DollyClient():
     
-    init_client()
-    message = input(" -> ")  # take input
+    def __init__(self, host, port):
+        self.port = port
+        self.host = host
+        self.client_socket = socket.socket()  # instantiate
+        self.client_socket.connect((host, port))  # connect to the server
+    
+    
+    def query_dolly(self, prompt):
+        self.client_socket.send(prompt.encode())
+        data = self.client_socket.recv(1024).decode()  # receive response
+    
+        return data
+    
+#def client_program():
+#    
+#    init_client()
+#    message = input(" -> ")  # take input
+#
+#    while message.lower().strip() != 'bye':
+#        data = query_dolly(message)  # receive response
+#
+#        print('Received from server: ' + data)  # show in terminal
+#
+#        message = input(" -> ")  # again take input
+#
+#    client_socket.close()  # close the connection
+#
 
-    while message.lower().strip() != 'bye':
-        data = query_dolly(message)  # receive response
-
-        print('Received from server: ' + data)  # show in terminal
-
-        message = input(" -> ")  # again take input
-
-    client_socket.close()  # close the connection
-
-
-if __name__ == '__main__':
-    client_program()
+#if __name__ == '__main__':
+#    client_program()
