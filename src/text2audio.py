7d232f9da12355ec2da57a8a7b4e513eda089047
Eden Mikitas <e.mikitas@gmail.com>
2023-06-26 23:33:12 +0300

    typo and male voice

--- src/text2audio.py
+++ src/text2audio.py
@@ -4,14 +4,16 @@ pip install pyttsx3
 '''
 
 import pyttsx3
+voice = 'english_rp+m1'
+
 
 def text_to_speech(sentence):
     engine = pyttsx3.init()
-    voice = engine.getProperty('voices')
+    #voice = engine.getProperty('voices')
     #engine.setProperty('voice', voice[11].id)
-    engine.setProperty('voice', 'english_rp+f3')
+    engine.setProperty('voice', voice) 
     engine.say(sentence)
     engine.runAndWait()
 
 if __name__ == '__main__':
-    text_to_speach()
+    text_to_speech("Hi! I'm dolly")
